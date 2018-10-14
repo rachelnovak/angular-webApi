bc7c19cfa00ef5a8beba393b9f8492105bf18e07
rachelnovak <rachel.novak@seldatinc.com>
2018-10-14 11:28:08 +0300

    client

--- Front-end/src/app/components/register/register.component.ts
+++ Front-end/src/app/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, ValidatorFn, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../../shared/services/user-service.service';
+import { createValidatorArr1, createValidatorArr2 } from '../../shared/validation/validation';
 
 
 @Component({
@@ -24,8 +25,8 @@ export class RegisterComponent {
 
   constructor(private userService: UserService, private router: Router) {
     let formGroupConfig = {
-      UserName: new FormControl("", this.createValidatorArr1("username",2,10)),
-      Age: new FormControl("", this.createValidatorArr2("age", 18, 120))
+      UserName: new FormControl("",createValidatorArr1("username",2,10)),
+      Age: new FormControl("",createValidatorArr2("age", 18, 120))
     };
 
     this.formGroup = new FormGroup(formGroupConfig);
@@ -41,20 +42,6 @@ export class RegisterComponent {
     this.userService.registerUserValid( this.formGroup.value);
   }
 
-  createValidatorArr1(cntName: string, min: number, max: number): Array<ValidatorFn> {
-    return [
-      f => !f.value ? { "val": `${cntName} is required` } : null,
-      f => f.value && f.value.length> max ? { "val": `${cntName} is max ${max} chars` } : null,
-      f => f.value && f.value.length<min? { "val": `${cntName} is min ${min} chars` } : null
-    ];
-  }
-  createValidatorArr2(cntName: string, min: number, max: number): Array<ValidatorFn> {
-    return [
-      f =>      !f.value ? { "val": `${cntName} is required` } : null,
-      f=>!Number(f.value)? {"val" : `${cntName} must be number`}:null,
-      f => f.value  && Number( f.value )&&f.value> max ? { "val": `${cntName} is max ${max} years` } : null,
-      f => f.value  && Number( f.value )&&f.value < min ? { "val": `${cntName} is min ${min} years` } : null
-    ];
-  }
+  
 }
 
